103c43e966b7d2cca184fe791c691e2e7d435ada
Shivang-Shrivastav <shivang.ahd@gmail.com>
2023-01-13 15:34:09 +0530

    Initial Commit

---
+++ data_process.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+import pickle
+
+df = pd.read_csv('df_data.csv')
+model = pickle.load(open('LGBM_Final_Model.pkl', 'rb'))
+
+X_test = df[df["date_block_num"]==34]
+X_test = X_test.drop("item_cnt_month", axis=1)
+
+
+
+def forecast(item_id,shop_id, X_test = X_test):
+
+  data = X_test[(X_test['shop_id'] == shop_id) &  (X_test['item_id'] == item_id) ]
+  result = model.predict(data, predict_disable_shape_check=True)
+  result = int(result)
+  return result
